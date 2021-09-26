597f1ff3fcac1b7de1e10cb98ea446665d99f661
no1sebomb <noisebombch@gmail.com>
2021-09-27 00:32:58 +0300

    Database init migration added

--- back/app/__init__.py
+++ back/app/__init__.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+from flask_migrate import Migrate
+
 from .utils import *
 from .server import create_app
 from .database import database
@@ -7,3 +9,4 @@ from .database import database
 
 app = create_app()
 database.init_app(app)
+migration = Migrate(app, database)
--- back/entrypoint.sh
+++ back/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 bash ./app/utils/update.sh
+flask db upgrade
 gunicorn wsgi:app -c gunicorn.py
